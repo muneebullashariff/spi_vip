12ab6cb55c3d5506b11a2e521dcfcf98658670b5
jagruthikappara <49189606+jagruthikappara@users.noreply.github.com>
2019-06-25 16:22:57 +0530

    Delete pkg.sv

--- uvm_tb/test/pkg.sv
+++
@@ -1,9 +0,0 @@
-
-
-	//Define type definition,functions and global variables for reuse
-
-	//import uvm_pkg using scope resolution
-
-	//include uvm_macros.svh
-
-	//include all the tb component files
