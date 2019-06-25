524ce41de1b00adde5fe01f959aeafc707ef784c
Maheswari3006 <48668226+Maheswari3006@users.noreply.github.com>
2019-06-25 17:19:41 +0530

    Update master_agent_top.sv

--- uvm_tb/master/master_agent_top.sv
+++ uvm_tb/master/master_agent_top.sv
@@ -19,33 +19,33 @@
 //
 //  ###########################################################################
 
-`ifndef _MASTER_AGENT_TOP_
-`define _MASTER_AGENT_TOP_
+`ifndef _MASTER_AGENT_TOP_INCLUDED_
+`define _MASTER_AGENT_TOP_INCLUDED_
 
 
 //-----------------------------------------------------------------------------
 // Class: master_agent_top
 // Description of the class.
 
-/*agent_top that is sub-environment container that may contain one or
-	more agents*/
+//agent_top that is sub-environment container that 
+//may contain one or more agents
 //------------------------------------------------------------------------------
 class master_agent_top extends uvm_env;
 
-/*register with factory so can use create uvm_method and override in
-	future if necessary*/ 
+//register with factory so can use create uvm_method and 
+//override in future if necessary 
  `uvm_component_utils(master_agent_top)
 
 //declare handle for agents and env config to be created
 master_agent m_agt[];
 env_config e_cfg;
 
-   //---------------------------------------------
-  // Externally defined tasks and functions
-  //---------------------------------------------
-  extern function new(string name = "master_agent_top", uvm_component parent); 
-  extern function void build_phase(uvm_phase phase);
-  extern task run_phase(uvm_phase phase);
+//---------------------------------------------
+// Externally defined tasks and functions
+//---------------------------------------------
+extern function new(string name = "master_agent_top", uvm_component parent); 
+extern function void build_phase(uvm_phase phase);
+extern task run_phase(uvm_phase phase);
 endclass: master_agent_top
 
 //-----------------------------------------------------------------------------
@@ -70,28 +70,25 @@ endfunction: new
 function void master_agent_top::build_phase(uvm_phase phase);
   
       if(!uvm_config_db #(env_config)::get(this," ","env_config",e_cfg))
+        begin
 	`uvm_fatal("TB CONFIG","cannot get() m_agt from uvm_config")
-
-       m_agt=new[e_cfg.no_of_magent];
+	end
+	
+        m_agt=new[e_cfg.no_of_magent];
        
          foreach(m_agt[i])
          begin
-              uvm_config_db #(master_agent_config)::set(this,$sformatf("m_agt[%0d]*",i),"master_agent_config",e_cfg.m_cfg[i]);
-              m_agt[i]=master_agent::type_id::create($sformatf("m_agt[%0d]",i),this);
+            uvm_config_db #(master_agent_config)::set(this,$sformatf("m_agt[%0d]*",i),"master_agent_config",e_cfg.m_cfg[i]);
+             m_agt[i]=master_agent::type_id::create($sformatf("m_agt[%0d]",i),this);
          end
   super.build_phase(phase);
 
 endfunction: build_phase
 //-------------------------------------------------------------------------------
 //Function:run task
-
+//-------------------------------------------------------------------------------
  task master_agent_top::run_phase(uvm_phase phase);
    uvm_top.print_topology;
  endtask:run_phase
-
-
-
-   
-
 
 `endif
