a58f0deb80aa1e7db031f05ca477a946576a5596
jagruthikappara <49189606+jagruthikappara@users.noreply.github.com>
2019-06-25 16:23:48 +0530

    Add files via upload

--- uvm_tb/test/test.sv
+++ uvm_tb/test/test.sv
@@ -1,90 +1,128 @@
+//  ###########################################################################
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
+//  ###########################################################################
+
+`ifndef _TEST_
+`define _TEST_
 
 
 
-	/*User-defined environment is derived from uvm_env, uvm_env is inherited from uvm_component.
-	Environment is the container class, It contains one or more agents or agent_tops,
-	as well as other components such as scoreboard,checker,virtual sequencer.
-	
-
-	//extend agent_top class from uvm_env
-	
-	
-	/*register with factory so can use create uvm_method and override in
-	future if necessary*/
-
-
-	//declare dynamic array of handles for master and slave agent_tops
-
-
-	//declare handles for virtual sequencer
-
-	
-	//declare handles for scoreboard
-
-	
-	//declare handle for environment config
-
-
-
-	/*User-defined test is derived from uvm_test, uvm_test is inherited from uvm_component.
+//-----------------------------------------------------------------------------
+// Class: test
+ /*User-defined test is derived from uvm_test, uvm_test is inherited from uvm_component.
 	Test defines the test scenario for the testbench,test class contains the environment, 
 	configuration properties, class overrides etc.
 	A sequence/sequences are created and started in the test*/
+//-----------------------------------------------------------------------------
+class test extends uvm_test;
 
+  `uvm_component_utils(test)
+  
+  env envh;
+  env_config e_cfg;
+  master_agent_config m_cfg[];
+  slave_agent_config s_cfg[];
+
+  int no_of_sagent = 1;
+  int no_of_magent = 1;
+  int has_m_agt = 1;
+  int has_s_agt = 1;
+
+
+  //---------------------------------------------
+  // Externally defined tasks and functions
+  //---------------------------------------------
+  extern function new(string name = "test", uvm_component parent); 
+  extern function void configuration();
+  extern function void build_phase(uvm_phase phase);
+  endclass:test 
+
+//-----------------------------------------------------------------------------
+// Constructor: new
+// Initializes the config_template class object
+//-----------------------------------------------------------------------------
+function test::new(string name = "test", uvm_component parent); 
+  super.new(name,parent); 
+endfunction: new 
+
+
+//-----------------------------------------------------------------------------
+// Function: config
+// Creates the required ports
+//-----------------------------------------------------------------------------
+function void test::configuration();
+  
+  uvm_config_db #(env_config)::set(this,"*","env_config",e_cfg);
+ 
+ if(has_m_agt)
+  begin
+
+     m_cfg = new[no_of_magent];
+     foreach(m_cfg[i])
+       begin
+         m_cfg[i] = master_agent_config::type_id::create($sformatf("m_cfg[%0d]",i));
+
+       end
+     end
+
+    
+  if(has_s_agt)
+  begin
+
+     s_cfg = new[no_of_sagent];
+     foreach(s_cfg[i])
+       begin
+         s_cfg[i] = slave_agent_config::type_id::create($sformatf("s_cfg[%0d]",i));
+
+       end
+     end
+		
+        e_cfg.no_of_magent = no_of_magent;
+        e_cfg.no_of_sagent = no_of_sagent;
+									 
+        e_cfg.has_m_agt = has_m_agt;
+        e_cfg.has_s_agt = has_s_agt;
+  endfunction 
+
+
+//-----------------------------------------------------------------------------
+// Function: build_phase
+// Creates the required ports
+//
+// Parameters:
+//  phase - stores the current phase 
+//-----------------------------------------------------------------------------
+function void test::build_phase(uvm_phase phase);
+ e_cfg = env_config::type_id::create("e_cfg");
+
+ 	if(has_m_agt)
+       		e_cfg.m_cfg = new[no_of_magent];
+          
+	if(has_s_agt)
+		e_cfg.s_cfg = new[no_of_sagent];
 	
-	//class extends uvm_test
+	configuration();
+	uvm_config_db #(env_config)::set(this,"*","env_config",e_cfg);
+    	super.build();
+	envh=env::type_id::create("envh", this);
+endfunction
 
-	
-	/*register with factory so can use create uvm_method and override in
-	future if necessary*/
-
-
-	//declare handles  for env_confgi and environment
-
-
-	//declare dynamic array of handles for master and slave config classes
-
-	
-	//declare local variables to override
-
-
-	
-	/*new constructor method that creates new component/object with
-	leaf_instance "string name" and handle to its parent class*/
-
-
-	/*build_phase necessary to get the automatic configuration of fields
-	registered in the component by calling super.build_phase(phase)*/
-	
-
-	//initialize the dynamic array of handles for master and slave cfg
-
-
-	//create instances of master and slave cfg
-
-
-	/*call get_config method,if value and the straing matches it returns
-	the settings made with previous set call,if falied stop the simulation*/ 
-
-
-	//assign master and slave config handles to env config handle
-
-
-	//override the is_active and other variables
-
-
-	//create the config object
-
-
-	//initialize the dynamic arrays of handles 
-
-	
-	//set the config object into uvm_config_db
-
-
-	//create instance of environment
-	
-
-
-	//extend different classes
-       
+  
+ `endif
---
+++ uvm_tb/test/test_pkg.sv
@@ -0,0 +1,33 @@
+package test_pkg;
+
+	import uvm_pkg::*;
+	`include "uvm_macros.svh"
+    
+    `include "master_xtn.sv"
+   `include "master_agent_config.sv"
+    `include "slave_agent_config.sv"
+    `include "env_config.sv"
+    `include "master_driver.sv"
+    `include "master_monitor.sv"    
+    `include "master_sequencer.sv"
+   `include "master_agent.sv"
+    `include "master_agent_top.sv"
+    `include "master_sequence.sv"
+
+   `include "slave_xtn.sv"
+    `include "slave_monitor.sv"
+    `include "slave_sequencer.sv"
+    `include "slave_sequence.sv"
+   `include "slave_driver.sv"
+    `include "slave_agent.sv"
+    `include "slave_agent_top.sv"
+
+   
+    //`include "scoreboard.sv"
+
+    `include "env.sv"
+
+
+    `include "test.sv"
+
+endpackage
